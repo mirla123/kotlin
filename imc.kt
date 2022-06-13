0126a713d9e9e0411dd24e315142ddee21179411
mirla123 <89169298+mirla123@users.noreply.github.com>
2022-06-13 20:41:33 -0300

    cálculo imc

---
+++ imc.kt
@@ -0,0 +1,17 @@
+import java.util.Scanner
+fun imc(peso:Float,altura:Float):Float{
+    return peso/(altura+altura)
+}
+fun main(){
+    val teclado= Scanner(System.`in`)
+    print("Digiteo peso:\t");
+    var peso:Float=teclado.nextFloat()
+    print("Digite a altura:\t")
+    var altura:Float=teclado.nextFloat()
+    if(imc(peso,altura)<18)
+    print("Sobpeso")
+    else if (imc(peso,altura)>18 || imc(peso,altura)<25)
+    print("Ok")
+    else
+    print("Sobpreso");
+}
