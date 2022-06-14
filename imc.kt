166968a801f5f16df603460b41470fcc71585aea
mirla123 <89169298+mirla123@users.noreply.github.com>
2022-06-13 22:35:44 -0300

    atividade

--- imc.kt
+++ imc.kt
@@ -1,17 +1,26 @@
 import java.util.Scanner
-fun imc(peso:Float,altura:Float):Float{
-    return peso/(altura+altura)
+
+fun imc(peso: Float, altura: Float): Float {
+    
+    return peso/(altura*altura)
 }
-fun main(){
-    val teclado= Scanner(System.`in`)
-    print("Digiteo peso:\t");
-    var peso:Float=teclado.nextFloat()
-    print("Digite a altura:\t")
-    var altura:Float=teclado.nextFloat()
-    if(imc(peso,altura)<18)
-    print("Sobpeso")
-    else if (imc(peso,altura)>18 || imc(peso,altura)<25)
-    print("Ok")
+
+fun main() {
+
+   val teclado = Scanner(System.`in`)
+   print("Digite o peso:\t")
+
+   var peso: Float = teclado.nextFloat()
+    print ("Digite a altura: \t")
+
+    var altura: Float = teclado.nextFloat()
+    
+    if (imc(peso,altura)<18)
+    print ("Sobpeso")
+
+    else if (imc (peso,altura)>18 && imc(peso,altura)<25)
+    print ("Ok")
+    
     else
-    print("Sobpreso");
+    print ("Sobrepeso");
 }
