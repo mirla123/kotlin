6e6b4aae46276c512a39da1c7fddaaa349ea7dd4
mirla123 <89169298+mirla123@users.noreply.github.com>
2022-06-20 22:23:35 -0300

    opa

--- vaquinha.kt
+++ vaquinha.kt
@@ -1,12 +1,20 @@
 import java.util.Scanner
+
 fun vaquinha(conta:Float, pessoas:Int):Float{
+
     return conta/pessoas;
 }
 fun main(){
     val teclado = Scanner(System.`in`)
+
     print("Digite o valor da conta:\t")
+
     var conta: Float = teclado.nextFloat()
+
     print("Digite a quantidades de pessoas.\t");
+
     var pessoas: Int= teclado.nextInt()
+
     println("cada pessoa :" + vaquinha(conta,pessoas) + " " + "R$");
+    
 }
